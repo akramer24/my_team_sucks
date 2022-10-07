d457d492ff0ba4af44a7d5f287bfa8ea1bdbf0a4
Ari Kramer <arikramer24@gmail.com>
2022-10-06 21:47:03 -0400

    chore: disable scheduled functions

--- functions/index.js
+++ functions/index.js
@@ -14,37 +14,37 @@ const mlbBot = new MlbBot(firestore);
 // });
 
 // Run every 5 minutes between 12am and 3am: https://crontab.guru/#*/5_0-3_*_*_*
-exports.processGamesLate = functions
-    .runWith({
-    // Ensure the function has enough memory and time
-    // to process large files
-      timeoutSeconds: 300,
-      memory: "4GB",
-    })
-    .pubsub.schedule("*/5 0-3 * * *")
-    .timeZone("America/New_York")
-    .onRun(async (context) => {
-      await mlbBot.processGames();
-      return null;
-    });
+// exports.processGamesLate = functions
+//     .runWith({
+//     // Ensure the function has enough memory and time
+//     // to process large files
+//       timeoutSeconds: 300,
+//       memory: "4GB",
+//     })
+//     .pubsub.schedule("*/5 0-3 * * *")
+//     .timeZone("America/New_York")
+//     .onRun(async (context) => {
+//       await mlbBot.processGames();
+//       return null;
+//     });
 
-// Run every 5 minutes between 2pm and 12am: https://www.adminschoice.com/crontab-quick-reference
-exports.processGamesEarly = functions
-    .runWith({
-      timeoutSeconds: 300,
-      memory: "4GB",
-    })
-    .pubsub.schedule("*/5 14-23 * * *")
-    .timeZone("America/New_York")
-    .onRun(async (context) => {
-      await mlbBot.processGames();
-      return null;
-    });
+// // Run every 5 minutes between 2pm and 12am: https://www.adminschoice.com/crontab-quick-reference
+// exports.processGamesEarly = functions
+//     .runWith({
+//       timeoutSeconds: 300,
+//       memory: "4GB",
+//     })
+//     .pubsub.schedule("*/5 14-23 * * *")
+//     .timeZone("America/New_York")
+//     .onRun(async (context) => {
+//       await mlbBot.processGames();
+//       return null;
+//     });
 
-// Run every morning at 9am: https://crontab.guru/#0_9_*_*_*
-exports.suckMeterReport = functions.pubsub.schedule("0 9 * * *")
-    .timeZone("America/New_York")
-    .onRun(async (context) => {
-      await mlbBot.suckMeterReport();
-      return null;
-    });
+// // Run every morning at 9am: https://crontab.guru/#0_9_*_*_*
+// exports.suckMeterReport = functions.pubsub.schedule("0 9 * * *")
+//     .timeZone("America/New_York")
+//     .onRun(async (context) => {
+//       await mlbBot.suckMeterReport();
+//       return null;
+//     });
